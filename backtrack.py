ad7234c05ed469677d26cfa4bb6c4fa186215945
ryker <rwall2@nebrwesleyn.edu>
2023-03-23 14:09:18 -0500

    Commit

--- backtrack.py
+++ backtrack.py
@@ -1,3 +1,5 @@
+import random
+
 def file_opener(name_file,distance_file):
     try:
         distances = open(distance_file)
@@ -40,13 +42,28 @@ all_routes = list()
 
 
 def tsp_backtrack():
+    remaining_cities = [0, 1, 2, 3, 4, 5, 6, 7]
     initial_route = list()
     for i in range(len(cities)):
         initial_route.append(i)
     initial_travel_time = route_distance_finder(initial_route)
-    print(initial_route)
-    return initial_travel_time
+    all_routes.append(initial_travel_time)
 
+    for n in range(0, 361):
+        new_route = list()
+        for num in remaining_cities:
+            new_route.append(num)
+            remaining_cities.remove(num)
+            for num1 in remaining_cities:
+                if num1 < num:
+                    remaining_cities.remove(num1)
+
+        print(new_route)
+        new_travel_time = route_distance_finder(new_route)
+        if new_travel_time < initial_travel_time:
+            all_routes.append(new_travel_time)
+
+    return min(all_routes)
 
 
 print(tsp_backtrack())
